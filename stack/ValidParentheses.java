b9e1955d2a3e5c27ebd9e89196039aa66a281949
Nam V. Do <namdvhe140597@fpt.edu.vn>
2020-02-12 13:29:04 +0700

    Valid Parentheses using Stack

--- stack/ValidParentheses.java
+++ stack/ValidParentheses.java
@@ -1,16 +1,18 @@
+package stack;
 import java.util.Stack;
+
 public class ValidParentheses {
     public static boolean isMatched (String expression){
         final String opening = "[{(";
         final String closing = "]})";
-        Stack <Charater> buffer = new Stack<>();
+        Stack <Character> buffer = new Stack<>();
         for(char c : expression.toCharArray()){
             if(opening.indexOf(c) != -1){
                 buffer.push(c);
             }else if(closing.indexOf(c) != -1){
                 if(buffer.empty()){
                     return false;
-                }if(closing.indexOf(c) != opening.indexOf(opening.pop())){
+                }if(closing.indexOf(c) != opening.indexOf(buffer.pop())){
                     return false;
                 }
             }
