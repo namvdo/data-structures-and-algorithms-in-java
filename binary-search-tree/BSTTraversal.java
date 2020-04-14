3cfba25b78989520d77504f94bdc7cab87707f87
Nam V. Do <vannam12a7@gmail.com>
2020-04-14 13:42:32 +0700

    Tree Traversal: Pre-order, In-order, Post-order

---
+++ binary-search-tree/BSTTraversal.java
@@ -0,0 +1,129 @@
+class BSTTraversal<T extends Comparable<T>> {
+    private Node root = null;
+    private int nodeCount = 0;
+
+    private class Node {
+        T element;
+        Node left, right;
+
+        public Node(Node left, Node right, T element) {
+            this.left = left;
+            this.right = right;
+            this.element = element;
+        }
+    }
+
+    /** Utility method for adding element to the BST. */
+    private Node insert(Node node, T value) {
+        if (node == null) {
+            node = new Node(null, null, value);
+        } else if (value.compareTo(node.element) < 0) {
+            node.left = insert(node.left, value);
+
+        } else if (value.compareTo(node.element) > 0) {
+            node.right = insert(node.right, value);
+        } else if (value.compareTo(node.element) == 0) {
+            return null;
+        }
+        return node;
+
+    }
+
+    public void add(T value) {
+        root = insert(root, value);
+        nodeCount++;
+    }
+
+    /**
+     * In-order traversal 
+     * 
+     * recursively explores the left subtree 
+     * visits the node
+     * recursive explores the right subtree
+     * 
+     * @param node - starting node
+     */
+    private void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.print(node.element + " ");
+            inOrder(node.right);
+        }
+
+    }
+
+    /** A method with no parameter for testing inOrder method */
+    public void inOrderTraversal() {
+        inOrder(root);
+    }
+
+    /**
+     * Pre-order traversal 
+     * 
+     * visits the node
+     * recursively explores the left subtree 
+     * recursive explores the right subtree
+     * 
+     * @param node - starting node
+     */
+    private void preOrder(Node node) {
+        if (node != null) {
+            System.out.print(node.element + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+
+    }
+
+    /** A method with no parameter for testing preOrder method */
+    public void preOrderTraversal() {
+        preOrder(root);
+    }
+
+    /**
+     * Post-order traversal 
+     * 
+     * recursively explores the left subtree 
+     * recursive explores the right subtree
+     * visits the node
+     * 
+     * @param node - starting node
+     */
+    private void postOrder(Node node) {
+        if (node != null) {
+            preOrder(node.left);
+            preOrder(node.right);
+            System.out.print(node.element + " ");
+        }
+
+    }
+
+    /** A method with no parameter for testing postOrder method */
+    public void postOrderTraversal() {
+        postOrder(root);
+    }
+
+    public static void main(String[] args) {
+        BSTTraversal<Integer> tree = new BSTTraversal<>();
+        tree.add(24);
+        tree.add(14);
+        tree.add(12);
+        tree.add(8);
+        tree.add(17);
+        tree.add(26);
+        tree.add(25);
+        tree.add(32);
+        System.out.println("In-order traversal: ");
+        tree.inOrderTraversal();
+        /** Output: 8 12 14 17 24 25 26 32  */
+        System.out.println();
+        System.out.println("Pre-order traversal: ");
+        tree.preOrderTraversal();
+        /** Output: 24 14 12 8 17 26 25 32  */
+        System.out.println();
+        System.out.println("Post-order traversal: ");
+        tree.postOrderTraversal();
+        /** Output: 14 12 8 17 26 25 32 24 */
+    }
+
+}
