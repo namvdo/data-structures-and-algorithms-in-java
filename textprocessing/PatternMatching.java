5fd3090ad095334d1861680aebba58f8725ad5a8
namvdo <vannam12a7@gmail.com>
2021-04-23 07:52:02 +0700

    Add pattern matching KMP algorithm

--- textprocessing/PatternMatching.java
+++ textprocessing/PatternMatching.java
@@ -42,4 +42,44 @@ public class PatternMatching {
         }
         return -1;
     }
+
+    public static int findKMP(char[] text, char[] pattern) {
+        int n = text.length;
+        int m = pattern.length;
+        if (m == 0) return 0;
+        int []fail = computeFailKMP(pattern);
+        int j = 0;
+        int k = 0;
+        while (j < n) {
+            if (text[j] == pattern[k]) {
+                if (k == m - 1) return j - m + 1;
+                j++;
+                k++;
+            } else if (k > 0) {
+                k = fail[k - 1];
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+
+    private static int[] computeFailKMP(char[] pattern) {
+        int m = pattern.length;
+        int []fail = new int[m];
+        int j = 1;
+        int k = 0;
+        while (j < m) {
+            if (pattern[j] == pattern[k]) {
+                fail[j] = k + 1;
+                j++;
+                k++;
+            } else if (k > 0) {
+                k = fail[k - 1];
+            } else {
+                j++;
+            }
+        }
+        return fail;
+    }
 }
